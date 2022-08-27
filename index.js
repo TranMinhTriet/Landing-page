4aa7c4561acf58edc0441ef956e38842d8746e02
Triet Tran <minhtriet.tr@gmail.com>
2022-08-27 12:18:12 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function clock() {
+    var days = document.getElementById("days");
+    var hours = document.getElementById("hour");
+    var minutes = document.getElementById("minutes");
+    var seconds = document.getElementById("seconds");
+  
+    var startDate = new Date("September 30, 2022 6:0:00").getTime();
+    var now = new Date().getTime();
+  
+    var Dday = startDate - now;
+  
+    var d = Math.floor(Dday / (1000 * 60 * 60 * 24));
+    var h = Math.floor(
+      (Dday % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var m = Math.floor((Dday % (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((Dday % (1000 * 60)) / 1000);
+  
+    days.innerHTML = d;
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+  }
+  
+  var interval = setInterval(clock, 1000);
+  
+  interval;
