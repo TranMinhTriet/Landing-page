96fa7d7ca85619046bfb40262796d20dcf559740
Tran Minh Triet <49344937+TranMinhTriet@users.noreply.github.com>
2024-11-13 09:57:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function clock() {
     var minutes = document.getElementById("minutes");
     var seconds = document.getElementById("seconds");
   
-    var startDate = new Date("September 30, 2022 6:0:00").getTime();
+    var startDate = new Date("November 30, 2024 6:0:00").getTime();
     var now = new Date().getTime();
   
     var Dday = startDate - now;
